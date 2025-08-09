c98da0fea7b5315da92119b359a644bdc59ec7f2
Nate Hansberry <grrr1981@gmail.com>
2025-08-09 08:49:34 +0200

    optimized

--- src/coercion.ts
+++ src/coercion.ts
@@ -1,14 +1,18 @@
-const startWorkTime = "09:00";
-const endWorkTime = "18:00";
+const startWorkTime = "07:30";
+const endWorkTime = "17:45";
 
 function scheduleMeeting(startMeetingTime: string, duration: number) {
-  const startWork = getHoursAndMinutes(startWorkTime);
-  const endWork = getHoursAndMinutes(endWorkTime);
   const startMeeting = getHoursAndMinutes(startMeetingTime);
   const durationObject = convertMinutesToHoursAndMinutes(duration);
   const endMeeting = calculateMeetingEndTime(startMeeting, durationObject);
+  if (notAString(startMeeting.hours) || notAString(startMeeting.minutes)) {
+    return false;
+  }
+  return canScheduleMeeting(startMeeting, endMeeting);
+}
 
-  return canScheduleMeeting(startWork, endWork, startMeeting, endMeeting);
+function notAString(time: string | null) {
+  return typeof time != "string"
 }
 
 function getHoursAndMinutes(startTime: string) {
@@ -47,19 +51,32 @@ function calculateMeetingEndTime(start: {hours: string, minutes: string}, durati
 }
 
 function canScheduleMeeting(
-  startWorkTime: {hours: string, minutes: string},
-  endWorkTime: {hours: string, minutes: string},
   startMeetingTime: {hours: string, minutes: string},
   endMeetingTime: {hours: string, minutes: string}
 ) {
-  const startWork = `${startWorkTime.hours.padStart(2,"0")}:${startWorkTime.minutes}`;
-  const endWork = `${endWorkTime.hours.padEnd(2,"0")}:${endWorkTime.minutes}`;
   const startMeeting = `${startMeetingTime.hours.padStart(2,"0")}:${startMeetingTime.minutes}`;
   const endMeeting = `${endMeetingTime.hours.padStart(2,"0")}:${endMeetingTime.minutes}`;
 
-  return startWork <= startMeeting && endWork >= endMeeting;
+  return startWorkTime <= startMeeting && endWorkTime >= endMeeting;
 }
 
-const canMeet = scheduleMeeting("18:15", 15);
+let canMeet = scheduleMeeting("7:00",15);
+console.log(`Is it possible to meet? : ${canMeet}`); // false
 
+canMeet = scheduleMeeting("07:15",30);    // false
+console.log(`Is it possible to meet? : ${canMeet}`);
+
+canMeet = scheduleMeeting("7:30",30);     // true
+console.log(`Is it possible to meet? : ${canMeet}`);
+
+canMeet = scheduleMeeting("11:30",60);    // true
+console.log(`Is it possible to meet? : ${canMeet}`);
+
+canMeet = scheduleMeeting("17:00",45);    // true
+console.log(`Is it possible to meet? : ${canMeet}`);
+
+canMeet = scheduleMeeting("17:30",30);    // false
+console.log(`Is it possible to meet? : ${canMeet}`);
+
+canMeet = scheduleMeeting("18:00",15);    // false
 console.log(`Is it possible to meet? : ${canMeet}`);
